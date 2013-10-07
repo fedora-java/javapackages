e2c7a4b7f93e5d401e55f5157ef9958ff98e391d
Michal Srb <msrb@redhat.com>
2013-10-07 14:45:07 +0200

    nosetests 0.10.4 doesn't have --ignore-files option

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nosetests --ignore-files=maven_prov_test_fuzzed.py
+nosetests --exclude=maven_prov_test_fuzzed.py
 
 if [ "x${1}" = "x--all" ]; then
     nosetests maven_prov_test_fuzzed.py
