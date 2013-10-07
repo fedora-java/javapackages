00ca7d0e80917a175d9e2dfe67e133e0fa1cbaa0
Michal Srb <msrb@redhat.com>
2013-10-07 17:59:37 +0200

    check: get exit code from a subshell

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 nosetests --exclude=maven_prov_test_fuzzed.py
+r1=$?
+r2=0
 
 if [ "x${1}" = "x--all" ]; then
     nosetests maven_prov_test_fuzzed.py
+    r2=$?
 fi
+
+if [ ${r1} -lt ${r2} ]; then
+    r1=${r2}
+fi
+
+exit ${r1}
 
