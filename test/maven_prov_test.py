73de066885cd4c4b1c32f584ee6474c30ff22530
Michael Simacek <msimacek@redhat.com>
2013-11-14 14:06:17 +0100

    [test] print stderr on test failure

--- test/maven_prov_test.py
+++ test/maven_prov_test.py
@@ -8,7 +8,7 @@ class TestMavenProv(unittest.TestCase):
 
     @mavenprov(["simple.xml"])
     def test_simple(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\n")
 
     @mavenprov(["invalid.xml"])
@@ -24,13 +24,13 @@ class TestMavenProv(unittest.TestCase):
         self.assertNotEqual(return_value, 0)
 
     @mavenprov(["single_ns.xml"])
-    def test_single_ns(self, stdout, stderrr, return_value):
-        self.assertEquals(return_value, 0)
+    def test_single_ns(self, stdout, stderr, return_value):
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\nns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["multi_ns.xml"])
-    def test_multi_ns(self, stdout, stderrr, return_value):
-        self.assertEquals(return_value, 0)
+    def test_multi_ns(self, stdout, stderr, return_value):
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\nns2-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["no_version.xml"])
@@ -39,7 +39,7 @@ class TestMavenProv(unittest.TestCase):
 
     @mavenprov(["simple.xml", "simple2.xml"])
     def test_more_files(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\nns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["two_in_one.xml"])
@@ -48,32 +48,32 @@ class TestMavenProv(unittest.TestCase):
 
     @mavenprov(["skip_provides.xml"])
     def test_skip_provides(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\n")
 
     @mavenprov(["non_compat.xml"])
     def test_non_compat(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory) = 1.0\n")
 
     @mavenprov(["extension1.xml"])
     def test_extension1(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:war:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["extension2.xml"])
     def test_extension2(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:war:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["pom_extension.xml"])
     def test__pom_extension(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["pom_namespace.xml"])
     def test_namespace_rhbz1017271(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "maven31-mvn(org.apache.maven:apache-maven:pom:) = 3.1.1\n"
                                   "maven31-mvn(org.apache.maven:apache-maven) = 3.1.1\n")
 
--- test/maven_req_test.py
+++ test/maven_req_test.py
@@ -20,48 +20,48 @@ class TestMavenReq(unittest.TestCase):
 
     @mavenreq(["require1/require.xml"])
     def test_require1(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "jpackage-utils\nmvn(org.apache.maven:maven-project)\n")
 
     @mavenreq(["require-java/require.xml"])
     def test_require_java(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "jpackage-utils\nmvn(org.apache.maven:maven-project)\njava >= 1:1.6\n")
 
     @mavenreq(["require-java-devel/require.xml"])
     def test_require_java_devel(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "jpackage-utils\nmvn(org.apache.maven:maven-project)\njava-devel >= 1:1.6\n")
 
     @mavenreq(["require-java-both/require.xml"])
     def test_require_java_both(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "jpackage-utils\nmvn(org.apache.maven:maven-project)\njava >= 1:1.6\njava-devel >= 1:1.6\n")
 
     @mavenreq(["require_parent/require.xml"])
     def test_require_parent(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-runtime\nns-mvn(org.codehaus.plexus:plexus-ant-factory)\n")
 
     @mavenreq(["require_multi/require.xml"])
     def test_require_multi(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-runtime\nns-mvn(org.codehaus.plexus:plexus-ant-factory)\nns-mvn(codehaus:plexus-utils) = 1.2\nmvn(org.apache.maven.wagon:wagon-provider-api::test-jar:)\n")
 
     @mavenreq(["require_multi_namespaces/require.xml"])
     def test_require_multi_namespace(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "jpackage-utils\nns-runtime\nns2-runtime\nns2-mvn(codehaus:plexus-cipher)\nns-mvn(codehaus:plexus-utils)\nmvn(org.apache.maven.wagon:wagon-provider-api::test-jar:)\n")
 
     @mavenreq(["require_multi_versioned/require.xml"])
     def test_require_multi_versioned(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-runtime\nns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\nns-mvn(codehaus:plexus-utils:1.2)\nns-mvn(codehaus:plexus-cipher:1.0) = 1.1\nmvn(org.apache.maven.wagon:wagon-provider-api::test-jar:)\nmvn(org.apache.maven.plugins:maven-idea-plugin:1.5) = 1.4\n")
 
 
     @mavenreq(["require_mixed/require.xml"])
     def test_mixed(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-runtime\nns-mvn(org.codehaus.plexus:plexus-ant-factory)\nns-mvn(codehaus:plexus-utils) = 1.2\nmvn(org.apache.maven.wagon:wagon-provider-api::test-jar:)\nmvn(org.apache.maven.plugins:maven-idea-plugin)\n")
 
     #test for rhbz#1012980
@@ -76,7 +76,7 @@ class TestMavenReq(unittest.TestCase):
     # rhbz#1017701 comment 2
     @mavenreq(["aether/require.xml"])
     def test_rhbz1017701_c2(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0)
+        self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "maven31-runtime\nmaven31-mvn(org.eclipse.aether:aether-api) = 0.9.0.M3\n")
 
 
