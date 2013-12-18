325b199faf1665d229f91fde8c2fc06b006dea47
Michael Simacek <msimacek@redhat.com>
2013-12-18 16:52:33 +0100

    [pom_editor.py] refactoring

--- java-utils/pom_editor.py
+++ java-utils/pom_editor.py
@@ -7,6 +7,9 @@ import sys
 from lxml import etree
 from os import path
 
+# all macro fuctions that can be called from external world
+macros = {}
+
 class PomException(Exception):
     pass
 class PomQueryNoMatch(PomException):
@@ -16,45 +19,33 @@ class PomQueryAmbigous(PomException):
 class PomQueryInvalid(PomException):
     pass
 
-class Artifact(object):
-    def __init__(self, specification, **defaults):
-        self.parts = specification.split(':')
-        self.values = dict.fromkeys(self.parts, '')
-        self.values.update(defaults)
-        print self.values
+def MetaArtifact(specification, **defaults):
+    parts = specification.split(':')
+    class Artifact(object):
+        def __init__(self, values):
+            self.values = dict.fromkeys(parts, '')
+            self.values.update(defaults)
+            for key, value in zip(parts, values.split(':')):
+                if value:
+                    self.values[key] = value
 
-    def __getitem__(self, name):
-        return self.values[name]
+        def get_xml(self):
+            xml = ''
+            for key in parts:
+                value = self.values[key]
+                if value:
+                    xml += "<{0}>{1}</{0}>".format(key, value)
+            return xml
 
-    def __setitem__(self, name, newval):
-        self.values[name] = newval
-
-    def load(self, string):
-        tokens = string.split(':')
-        if len(tokens) > len(self.parts):
-            raise PomException("Too many colons in artifact specification '{0}'."\
-                                .format(string))
-        for i, token in enumerate(tokens):
-            if token:
-                self.values[self.parts[i]] = token
-        return self
-
-    def get_xml(self):
-        xml = ''
-        for key in self.parts:
-            value = self.values[key]
-            if value:
-                xml += "<{0}>{1}</{0}>".format(key, value)
-        return xml
-
-    def get_xpath_condition(self):
-        expr = "normalize-space(pom:{0})='{1}'"
-        conditions = []
-        for key in self.parts:
-            value = self.values[key]
-            if value and value != '*':
-                conditions.append(expr.format(key, value))
-        return ' and '.join(conditions)
+        def get_xpath_condition(self):
+            expr = "normalize-space(pom:{0})='{1}'"
+            conditions = []
+            for key in parts:
+                value = self.values[key]
+                if value and value != '*':
+                    conditions.append(expr.format(key, value))
+            return ' and '.join(conditions)
+    return Artifact
 
 def find_pom(pompath):
     if path.isfile(pompath):
@@ -72,37 +63,47 @@ def get_indent(node):
     text = node.text.split('\n')[-1]
     return re.sub(r'\S.*', '', text)
 
-def patch_poms(function, args):
-    (arglist, _, _, defaults) = inspect.getargspec(function)
-    maxargcount = len(arglist)
-    minargcount = maxargcount - len(defaults or [])
-    if len(args) > maxargcount or len(args) < minargcount:
-        print >> sys.stderr, "Usage: %pom_{name} {doc}".format(name=function.__name__,
-                                                         doc=function.__doc__)
-        sys.exit(1)
-    pomspec_pos = arglist.index('pom')
-    if pomspec_pos < len(args):
-        pomspec = args[pomspec_pos]
-    else:
-        pomspec = 'pom.xml'
-    try:
-        pompath = find_pom(pomspec)
-    except PomException as exception:
-        print >> sys.stderr, exception.message
-        sys.exit(2)
-    try:
-        pomdir = path.dirname(pompath) or '.'
-        pom = Pom(pompath)
-        pom.write(path.join(pomdir, path.basename(pompath) + '.tmp'))
-        fnargs = args[:pomspec_pos] + [pom] + args[pomspec_pos + 1:]
-        function(*fnargs)
-        origfile = path.join(pomdir, path.basename(pompath) + '.orig')
-        shutil.move(pompath, origfile)
-        pom.write(pompath)
-    except (PomException, etree.XMLSyntaxError, OSError, IOError) as exception:
-        print >> sys.stderr, "Error in processing {0}".format(pompath)
-        print >> sys.stderr, exception.message
-        sys.exit(3)
+def macro(outer_function):
+    def decorator(function):
+        def decorated(*args):
+            (arglist, _, _, defaults) = inspect.getargspec(function)
+            maxargcount = len(arglist)
+            minargcount = maxargcount - len(defaults or [])
+            if len(args) > maxargcount or len(args) < minargcount:
+                print >> sys.stderr, "Usage: %pom_{name} {doc}".format(name=function.__name__,
+                                                                 doc=function.__doc__)
+                sys.exit(1)
+            pomspec_pos = arglist.index('pom')
+            if pomspec_pos < len(args):
+                pomspec = args[pomspec_pos]
+            else:
+                pomspec = 'pom.xml'
+
+            try:
+                pompath = find_pom(pomspec)
+            except PomException as exception:
+                print >> sys.stderr, exception.message
+                sys.exit(2)
+
+            try:
+                pomdir = path.dirname(pompath) or '.'
+                pom = Pom(pompath)
+                pom.write(path.join(pomdir, path.basename(pompath) + '.tmp'))
+                print args
+                print args[:pomspec_pos], args[pomspec_pos:]
+                fnargs = list(args[:pomspec_pos]) + [pom] + list(args[pomspec_pos + 1:])
+                function(*fnargs)
+                origfile = path.join(pomdir, path.basename(pompath) + '.orig')
+                shutil.move(pompath, origfile)
+                pom.write(pompath)
+            except (PomException, etree.XMLSyntaxError, OSError, IOError) as exception:
+                print >> sys.stderr, "Error in processing {0}".format(pompath)
+                print >> sys.stderr, exception.message
+                sys.exit(3)
+
+        macros[function.__name__] = decorated
+        return decorated
+    return decorator(outer_function)
 
 class Pom(object):
     NSMAP = {'pom': 'http://maven.apache.org/POM/4.0.0',
@@ -227,97 +228,102 @@ class Pom(object):
     def comment(content):
         return Pom.BEGIN_COMMENT + content + Pom.END_COMMENT
 
-class PomEditor(object):
-    @staticmethod
-    def pom_xpath_inject(where, xml_string, pom=None):
-        pom.inject_xml(pom.xpath_query_element(where), Pom.comment(xml_string))
+artifact_spec = 'groupId:artifactId:version'
+artifact_spec_scoped = artifact_spec + ':scope'
+VersionedArtifact = MetaArtifact(artifact_spec)
+DefaultVersionedArtifact = MetaArtifact(artifact_spec, version='any')
+ScopedArtifact = MetaArtifact(artifact_spec_scoped, version='any')
 
-    @staticmethod
-    def pom_xpath_replace(where, xml_string, pom=None):
-        pom.replace_xml(pom.xpath_query_element(where), Pom.comment(xml_string))
+@macro
+def pom_xpath_inject(where, xml_string, pom=None):
+    pom.inject_xml(pom.xpath_query_element(where), Pom.comment(xml_string))
 
-    @staticmethod
-    def pom_xpath_remove(where, pom=None):
-        pom.replace_xml(pom.xpath_query_element(where), "<!-- element removed by maintainer -->")
+@macro
+def pom_xpath_replace(where, xml_string, pom=None):
+    pom.replace_xml(pom.xpath_query_element(where), Pom.comment(xml_string))
 
-    @staticmethod
-    def pom_xpath_set(where, content, pom=None):
-        #TODO check content
-        pom.replace_xml_content(pom.xpath_query_element(where), Pom.comment(content))
+@macro
+def pom_xpath_remove(where, pom=None):
+    pom.replace_xml(pom.xpath_query_element(where), "<!-- element removed by maintainer -->")
 
-    @staticmethod
-    def pom_remove_dep(dep, pom=None):
-        try:
-            artifact = Artifact('groupId:artifactId:version').load(dep)
-            xpath = "//pom:dependency[{0}]".format(artifact.get_xpath_condition())
-            elements = pom.xpath_query(xpath)
-            for element in elements:
-                pom.replace_xml(element, "<!-- dependency removed by maintainer -->")
-        except PomQueryNoMatch:
-            raise PomException("Dependency '{0}' not found.".format(dep))
+@macro
+def pom_xpath_set(where, content, pom=None):
+    #TODO check content
+    pom.replace_xml_content(pom.xpath_query_element(where), Pom.comment(content))
 
-    @staticmethod
-    def pom_remove_plugin(plugin, pom=None):
-        try:
-            artifact = Artifact('groupId:artifactId:version').load(plugin)
-            xpath = "//pom:plugin[{0}]".format(artifact.get_xpath_condition())
-            elements = pom.xpath_query(xpath)
-            for element in elements:
-                pom.replace_xml(element, "<!-- plugin removed by maintainer -->")
-        except PomQueryNoMatch:
-            raise PomException("Plugin '{0}' not found.".format(plugin))
+@macro
+def pom_remove_dep(dep, pom=None):
+    try:
+        artifact = VersionedArtifact(dep)
+        xpath = "//pom:dependency[{0}]".format(artifact.get_xpath_condition())
+        elements = pom.xpath_query(xpath)
+        for element in elements:
+            pom.replace_xml(element, "<!-- dependency removed by maintainer -->")
+    except PomQueryNoMatch:
+        raise PomException("Dependency '{0}' not found.".format(dep))
 
-    @staticmethod
-    def pom_disable_module(module, pom=None):
-        try:
-            xpath = "//pom:module[normalize-space(text())='{0}']".format(module)
-            elements = pom.xpath_query(xpath)
-            for element in elements:
-                pom.replace_xml(element, "<!-- module removed by maintainer -->")
-        except PomQueryNoMatch:
-            raise PomException("Module '{0}' not found.".format(module))
+@macro
+def pom_remove_plugin(plugin, pom=None):
+    try:
+        artifact = VersionedArtifact(plugin)
+        xpath = "//pom:plugin[{0}]".format(artifact.get_xpath_condition())
+        elements = pom.xpath_query(xpath)
+        for element in elements:
+            pom.replace_xml(element, "<!-- plugin removed by maintainer -->")
+    except PomQueryNoMatch:
+        raise PomException("Plugin '{0}' not found.".format(plugin))
 
-    @staticmethod
-    def pom_add_parent(parent, pom=None):
-        if pom.root.find('pom:parent', namespaces=Pom.NSMAP) is not None:
-            raise PomException("POM already has a parent.")
-        artifact = Artifact('groupId:artifactId:version', version='any').load(parent)
-        pom.inject_artifact('', 'parent', artifact)
+@macro
+def pom_disable_module(module, pom=None):
+    try:
+        xpath = "//pom:module[normalize-space(text())='{0}']".format(module)
+        elements = pom.xpath_query(xpath)
+        for element in elements:
+            pom.replace_xml(element, "<!-- module removed by maintainer -->")
+    except PomQueryNoMatch:
+        raise PomException("Module '{0}' not found.".format(module))
 
-    @staticmethod
-    def pom_remove_parent(pom=None):
-        try:
-            pom.replace_xml(pom.xpath_query_element("/pom:project/pom:parent"),
-                            "<!-- parent POM reference removed by maintainer -->")
-        except PomQueryNoMatch:
-            raise PomException("POM doesn't specify parent.")
+@macro
+def pom_add_parent(parent, pom=None):
+    if pom.root.find('pom:parent', namespaces=Pom.NSMAP) is not None:
+        raise PomException("POM already has a parent.")
+    artifact = DefaultVersionedArtifact(parent)
+    pom.inject_artifact('', 'parent', artifact)
 
-    @staticmethod
-    def pom_set_parent(parent, pom=None):
-        try:
-            artifact = Artifact('groupId:artifactId:version:scope', version='any').load(parent)
-            element = pom.xpath_query_element("/pom:project/pom:parent")
-            pom.replace_xml_content(element, artifact.get_xml())
-        except PomQueryNoMatch:
-            raise PomException("POM doesn't specify parent.")
+@macro
+def pom_remove_parent(pom=None):
+    try:
+        pom.replace_xml(pom.xpath_query_element("/pom:project/pom:parent"),
+                        "<!-- parent POM reference removed by maintainer -->")
+    except PomQueryNoMatch:
+        raise PomException("POM doesn't specify parent.")
 
-    @staticmethod
-    def pom_add_dep(dep, pom=None, xml_string=''):
-        artifact = Artifact('groupId:artifactId:version:scope', version='any').load(dep)
-        pom.inject_artifact('pom:dependencies', 'dependency', artifact,
-                            xml_string)
+@macro
+def pom_set_parent(parent, pom=None):
+    try:
+        artifact = ScopedArtifact(parent)
+        element = pom.xpath_query_element("/pom:project/pom:parent")
+        pom.replace_xml_content(element, artifact.get_xml())
+    except PomQueryNoMatch:
+        raise PomException("POM doesn't specify parent.")
 
-    @staticmethod
-    def pom_add_dep_mgmt(dep, pom=None, xml_string=''):
-        artifact = Artifact('groupId:artifactId:version:scope', version='any').load(dep)
-        pom.inject_artifact('pom:dependencyManagement/pom:dependencies',
-                            'dependency', artifact, xml_string)
+@macro
+def pom_add_dep(dep, pom=None, xml_string=''):
+    artifact = ScopedArtifact(dep)
+    pom.inject_artifact('pom:dependencies', 'dependency', artifact,
+                        xml_string)
 
-    @staticmethod
-    def pom_add_plugin(plugin, pom=None):
-        artifact = Artifact('groupId:artifactId:version', version='any',
-                            groupId='org.apache.maven.plugins').load(plugin)
-        pom.inject_artifact('pom:build/pom:plugins', 'plugin', artifact)
+@macro
+def pom_add_dep_mgmt(dep, pom=None, xml_string=''):
+    artifact = ScopedArtifact(dep)
+    pom.inject_artifact('pom:dependencyManagement/pom:dependencies',
+                        'dependency', artifact, xml_string)
+
+@macro
+def pom_add_plugin(plugin, pom=None):
+    artifact = MetaArtifact(artifact_spec, version='any',
+                        groupId='org.apache.maven.plugins')(plugin)
+    pom.inject_artifact('pom:build/pom:plugins', 'plugin', artifact)
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -325,5 +331,4 @@ if __name__ == '__main__':
                               .format(sys.argv[0])
         sys.exit(1)
 
-    command = getattr(PomEditor, sys.argv[1])
-    patch_poms(command, sys.argv[2:])
+    macros[sys.argv[1]](*sys.argv[2:])
