23e05125fe2f08d400d8f2ea71bf73cfdac5df8c
Michael Simacek <msimacek@redhat.com>
2014-02-27 15:59:32 +0100

    [test] don't fail on scripts with future imports

--- test/wrapper.py
+++ test/wrapper.py
@@ -1,13 +1,28 @@
 import sys
 import subprocess
 import os
+import textwrap
 
-PYTHONPATH = os.environ['PYTHONPATH'].split(':')
-script = 'import sys; sys.path=["{pythonpath}"]+sys.path;'.format(pythonpath=','.join(PYTHONPATH))
+if 'PYTHONPATH' in os.environ:
+    PYTHONPATH = os.environ['PYTHONPATH'].split(':')
+else:
+    PYTHONPATH = "."
+
+injected = textwrap.dedent("""\
+        import sys as ____sys
+        ____sys.path = ["{pythonpath}"] + ____sys.path
+        """.format(pythonpath='" ,"'.join(PYTHONPATH)))
+
 with open(sys.argv[1], 'r') as scriptfile:
-    script += scriptfile.read()
+    script = scriptfile.read().split('\n')
+
+futures = [idx for idx, line in enumerate(script) if '__future__' in line]
+index = futures[-1] if futures else 0
+index += 1
+script[index:index] = injected.split('\n')
+
 with open('.script.py', 'w') as newscript:
-    newscript.write(script)
+    newscript.write('\n'.join(script))
 
 proc = subprocess.Popen([sys.executable, '.script.py'] + sys.argv[2:])
 sys.exit(proc.wait())
