67b450bb711f81bc667bc84170f4f0c2bd4f64f5
Michael Simacek <msimacek@redhat.com>
2014-03-14 16:35:09 +0100

    [maven.req] try to provide more helpful error messages

--- test/maven_req_test.py
+++ test/maven_req_test.py
@@ -71,7 +71,9 @@ class TestMavenReq(unittest.TestCase):
         self.assertNotEqual(stderr, '')
         lines = stderr.split('\n');
         self.assertEquals(True, len(lines) > 1)
-        self.assertEquals(lines[-2], '%mvn_package org.codehaus.plexus:plexus-ant-factory:::1.0 <package_name>')
+        print stderr
+        self.assertEquals(lines[-4], 'org.codehaus.plexus:plexus-ant-factory:::1.0 ' \
+                                     'required by org.apache.commons-lang:commons-lang')
 
     # rhbz#1017701 comment 2
     @mavenreq(["aether/require.xml"])
