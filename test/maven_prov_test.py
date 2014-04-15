bfd122220aa2202d7138bd0edc754a77e04ee1b5
Michal Srb <msrb@redhat.com>
2014-04-15 13:03:43 +0200

    [test] Fix remaining tests for maven.prov

--- test/maven_prov_test.py
+++ test/maven_prov_test.py
@@ -28,13 +28,16 @@ class TestMavenProv(unittest.TestCase):
         self.assertEquals(return_value, 0, stderr)
         self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\n"
                           "ns-mvn(org.codehaus.plexus:plexus-ant-factory:pom:1.0) = 1.0\n"
-                          "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:1.0) = 9.1.1.B60.25.p2\n"
+                          "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:1.0) = 9.1.1.b60.25.p2\n"
                           "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:pom:1.0) = 9.1.1.b60.25.p2\n")
 
     @mavenprov(["multi_ns.xml"])
     def test_multi_ns(self, stdout, stderr, return_value):
         self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\nns2-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
+        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\n"
+                          "ns-mvn(org.codehaus.plexus:plexus-ant-factory:pom:1.0) = 1.0\n"
+                          "ns2-mvn(org.mortbay.jetty:jsp-2.1-glassfish:1.0) = 9.1.1.b60.25.p2\n"
+                          "ns2-mvn(org.mortbay.jetty:jsp-2.1-glassfish:pom:1.0) = 9.1.1.b60.25.p2\n")
 
     @mavenprov(["no_version.xml"])
     def test_no_version(self, stdout, stderr, return_value):
@@ -43,21 +46,19 @@ class TestMavenProv(unittest.TestCase):
     @mavenprov(["simple.xml", "simple2.xml"])
     def test_more_files(self, stdout, stderr, return_value):
         self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\nns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
+        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\n"
+                          "ns-mvn(org.codehaus.plexus:plexus-ant-factory:pom:1.0) = 1.0\n"
+                          "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.b60.25.p2\n"
+                          "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:pom:6.0.18) = 9.1.1.b60.25.p2\n")
 
     @mavenprov(["two_in_one.xml"])
     def test_two_in_one(self, stdout, stderr, return_value):
         self.assertNotEqual(return_value, 0)
 
-    @mavenprov(["skip_provides.xml"])
-    def test_skip_provides(self, stdout, stderr, return_value):
-        self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory:1.0) = 1.0\n")
-
     @mavenprov(["non_compat.xml"])
     def test_non_compat(self, stdout, stderr, return_value):
         self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory) = 1.0\n")
+        self.assertEquals(stdout, "ns-mvn(org.codehaus.plexus:plexus-ant-factory) = 1.0\nns-mvn(org.codehaus.plexus:plexus-ant-factory:pom:) = 1.0\n")
 
     @mavenprov(["extension1.xml"])
     def test_extension1(self, stdout, stderr, return_value):
@@ -67,7 +68,7 @@ class TestMavenProv(unittest.TestCase):
     @mavenprov(["extension2.xml"])
     def test_extension2(self, stdout, stderr, return_value):
         self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:war:6.0.18) = 9.1.1.B60.25.p2\n")
+        self.assertEquals(stdout, "ns-mvn(org.mortbay.jetty:jsp-2.1-glassfish:6.0.18) = 9.1.1.B60.25.p2\n")
 
     @mavenprov(["pom_extension.xml"])
     def test__pom_extension(self, stdout, stderr, return_value):
@@ -77,14 +78,12 @@ class TestMavenProv(unittest.TestCase):
     @mavenprov(["pom_namespace.xml"])
     def test_namespace_rhbz1017271(self, stdout, stderr, return_value):
         self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "maven31-mvn(org.apache.maven:apache-maven:pom:) = 3.1.1\n"
-                                  "maven31-mvn(org.apache.maven:apache-maven) = 3.1.1\n")
+        self.assertEquals(stdout, "maven31-mvn(org.apache.maven:apache-maven:pom:) = 3.1.1\n")
 
     @mavenprov(["pom_compat.xml"])
     def test_compat_version_in_artifact(self, stdout, stderr, return_value):
         self.assertEquals(return_value, 0, stderr)
-        self.assertEquals(stdout, "mvn(org.apache.maven:apache-maven:pom:3.1.1) = 3.1.1\n"
-                "mvn(org.apache.maven:apache-maven:3.1.1) = 3.1.1\n")
+        self.assertEquals(stdout, "mvn(org.apache.maven:apache-maven:pom:3.1.1) = 3.1.1\n")
 
 if __name__ == '__main__':
     unittest.main()
