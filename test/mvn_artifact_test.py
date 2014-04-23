50852f05a87b882ee0da857e814fd9fc0ab4efc9
Michal Srb <msrb@redhat.com>
2014-04-23 08:59:25 +0200

    [test] Add test with exclusions

--- test/mvn_artifact_test.py
+++ test/mvn_artifact_test.py
@@ -144,5 +144,11 @@ class TestMvnArtifact(unittest.TestCase):
         got, want, res = self.check_result(inspect.currentframe().f_code.co_name)
         self.assertEqual(res, True)
 
+    @mvn_artifact('args4j-exclusions.pom', 'some.jar')
+    def test_dependencies_exclusions(self, stdout, stderr, return_value):
+        self.assertEqual(return_value, 0, stderr)
+        got, want, res = self.check_result(inspect.currentframe().f_code.co_name)
+        self.assertEqual(res, True)
+
 if __name__ == '__main__':
     unittest.main()
