f61bb74a4f57730c1d93255713a7701a5792c59e
Michal Srb <msrb@redhat.com>
2014-06-09 20:31:02 +0200

    [test] Try to generate xunit file only with newer nosetests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-PYTHONPATH="../python" nosetests --with-xunit --exclude="(maven_prov)|(maven_depmap)|(mvn_artifact)_test.py"
+xunit_param=""
+nose_version=`nosetests --version | awk '{ print $3}' | awk -F'.' '{print $1}'`
+if [[ ${nose_version} -ge 1 ]]; then
+    xunit_param="--with-xunit"
+fi
+
+PYTHONPATH="../python" nosetests ${xunit_param} --exclude="(maven_prov)|(maven_depmap)|(mvn_artifact)_test.py"
 r1=$?
 r2=0
 
