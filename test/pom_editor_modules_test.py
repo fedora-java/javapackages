101996636f915a9e365bc6e609d2c6990be26b22
Michael Simacek <msimacek@redhat.com>
2014-07-03 13:59:43 +0200

    More tests for pom_xpath_disable

--- test/pom_editor_modules_test.py
+++ test/pom_editor_modules_test.py
@@ -22,5 +22,32 @@ class TestPomXpathDisable(WorkdirTestCase):
         self.assertEqual(0, return_value, stderr)
         self.assertEqual('', report, report)
 
+    def test_two_levels(self):
+        return_value, stderr, report = exec_pom_macro(
+                """%pom_xpath_disable "pom:parent[pom:artifactId='intermediate']" """,
+               poms_tree=example_tree,
+               want_tree={'intermediate': 'intermediate_disabled_schemas.xml'})
+        self.assertEqual(0, return_value, stderr)
+        self.assertEqual('', report, report)
+
+    def test_skip_level(self):
+        poms_tree = dict(example_tree)
+        del poms_tree['intermediate/schemas']
+        poms_tree['intermediate'] = 'intermediate_skip.xml'
+        poms_tree['intermediate/src/schemas'] = 'example_schemas.xml'
+        return_value, stderr, report = exec_pom_macro(
+                """%pom_xpath_disable "pom:project.build.sourceEncoding='UTF-8'" """,
+               poms_tree=poms_tree,
+               want_tree={'intermediate': 'intermediate_disabled_schemas.xml'})
+        self.assertEqual(0, return_value, stderr)
+        self.assertEqual('', report, report)
+
+    def test_fail_on_main(self):
+        return_value, stderr, _ = exec_pom_macro(
+                """%pom_xpath_disable "pom:groupId='com.example'" """,
+               poms_tree=example_tree)
+        assertIn(self, "Main POM satisfies the condition", stderr)
+        self.assertNotEqual(0, return_value)
+
 if __name__ == '__main__':
     unittest.main()
