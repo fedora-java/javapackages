788f744ec674edc254545775e0ad409c9f8ed253
Michal Srb <msrb@redhat.com>
2014-07-21 08:05:03 +0200

    [test] Add basic test for osgi.prov

--- test/osgi_prov_test.py
+++ test/osgi_prov_test.py
@@ -1,8 +1,7 @@
-import os
-import sys
 import unittest
 
 from test_common import *
+
 
 class TestOSGiProv(unittest.TestCase):
 
@@ -11,6 +10,12 @@ class TestOSGiProv(unittest.TestCase):
         self.assertEquals(return_value, 0, stderr)
         sout = [x for x in stdout.split('\n') if x]
         self.assertEquals(len(sout), 0)
+
+    @osgiprov(["data/osgi/basic/META-INF/MANIFEST.MF"])
+    def test_basic(self, stdout, stderr, return_value):
+        self.assertEquals(return_value, 0, stderr)
+        sout = [x for x in stdout.split('\n') if x]
+        assertIn(self, "osgi(org.junit) = 4.10.0", sout)
 
 if __name__ == '__main__':
     unittest.main()
