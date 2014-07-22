662a1b04dd807c58f698c48be800cc366901b7bc
Michal Srb <msrb@redhat.com>
2014-07-22 09:20:31 +0200

    [test] Fix "ResourceWarning: unclosed file"

--- test/test_common.py
+++ test/test_common.py
@@ -17,24 +17,32 @@ SCRIPT_ENV = {'PATH':'{mock}:{real}'.format(mock=DIRPATH,
                                             real=os.environ['PATH']),
               'PYTHONPATH':PYTHONPATH}
 
+
 def call_script(name, args, stdin=None, wrapped=False, extra_env={}, config_path=''):
-    outfile = open("tmpout", 'w')
-    errfile = open("tmperr", 'w')
-    procargs = [sys.executable, path.join(DIRPATH, 'wrapper.py'), name, config_path]
-    env = SCRIPT_ENV.copy()
-    env.update(extra_env)
-    proc = subprocess.Popen(procargs + args, shell=False,
-        stdout=outfile, stderr=errfile, env=env,
-        stdin=subprocess.PIPE, universal_newlines=True)
-    proc.communicate(stdin)
-    ret = proc.wait()
-    outfile = open("tmpout", 'r+')
-    errfile = open("tmperr", 'r+')
-    out = outfile.read()
-    err = errfile.read()
+    with open("tmpout", 'w') as outfile:
+        with open("tmperr", 'w') as errfile:
+            procargs = [sys.executable,
+                        path.join(DIRPATH, 'wrapper.py'),
+                        name,
+                        config_path]
+            env = SCRIPT_ENV.copy()
+            env.update(extra_env)
+            proc = subprocess.Popen(procargs + args, shell=False,
+                                    stdout=outfile,
+                                    stderr=errfile,
+                                    env=env,
+                                    stdin=subprocess.PIPE,
+                                    universal_newlines=True)
+            proc.communicate(stdin)
+            ret = proc.wait()
+    with open("tmpout", 'r') as outfile:
+        out = outfile.read()
+    with open("tmperr", 'r') as errfile:
+        err = errfile.read()
     os.remove('tmpout')
     os.remove('tmperr')
     return (out, err, ret)
+
 
 def get_config_file_list():
     try:
@@ -85,7 +93,8 @@ def prepare_metadata(metadata_dir):
         for filename in filenames:
             if filename.endswith("-want.xml"):
                 want_file = os.path.join(dirname, filename)
-                metadata = m.CreateFromDocument(open(want_file).read())
+                with open(want_file) as wfile:
+                    metadata = m.CreateFromDocument(wfile.read())
                 for a in metadata.artifacts.artifact:
                     if '%' in a.path:
                         a.path = a.path % (metadata_dir)
