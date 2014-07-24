7e40493e0e074c0dd0672aaa7aaefed8bf58f435
Michal Srb <msrb@redhat.com>
2014-07-24 09:29:07 +0200

    [test] Two more tests for OSGi (empty manifest)

--- test/osgi_prov_test.py
+++ test/osgi_prov_test.py
@@ -23,5 +23,12 @@ class TestOSGiProv(unittest.TestCase):
         sout = [x for x in stdout.split('\n') if x]
         assertIn(self, "osgi(org.junit) = 4.10.0", sout)
 
+    @osgiprov(["data/osgi/empty/META-INF/MANIFEST.MF"])
+    def test_empty(self, stdout, stderr, return_value):
+        self.assertEquals(return_value, 0, stderr)
+        sout = [x for x in stdout.split('\n') if x]
+        self.assertEquals(len(sout), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
--- test/osgi_req_test.py
+++ test/osgi_req_test.py
@@ -23,5 +23,11 @@ class TestOSGiReq(unittest.TestCase):
         sout = [x for x in stdout.split('\n') if x]
         assertIn(self, "osgi(org.hamcrest.core)", sout)
 
+    @osgireq(["data/osgi/empty/META-INF/MANIFEST.MF"])
+    def test_empty(self, stdout, stderr, return_value):
+        self.assertEquals(return_value, 0, stderr)
+        sout = [x for x in stdout.split('\n') if x]
+        self.assertEquals(len(sout), 0)
+
 if __name__ == '__main__':
     unittest.main()
