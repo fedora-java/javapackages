d35af95c5cba1b898a9b183076d1a88c534415b9
Michael Simacek <msimacek@redhat.com>
2014-10-21 10:23:08 +0200

    [pom_editor] Fix missing space between xmlns declarations

--- java-utils/pom_editor.py
+++ java-utils/pom_editor.py
@@ -350,8 +350,8 @@ class Pom(XmlFile):
     NSMAP = {'pom': 'http://maven.apache.org/POM/4.0.0',
               'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
     NS = '{' + NSMAP['pom'] + '}'
-    XMLNS = ('xmlns="http://maven.apache.org/POM/4.0.0"'
-    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+    XMLNS = ('xmlns="http://maven.apache.org/POM/4.0.0" '
+    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
     'xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 '
     'http://maven.apache.org/xsd/maven-4.0.0.xsd"')
     DEPENDENCY_NODE = 'pom:dependency'
