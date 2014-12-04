a1c7e5e2f65fbfc637188c32621379c0ead16cdd
Michal Srb <msrb@redhat.com>
2014-12-04 09:04:14 +0100

    [test] Fix OSGi tests

--- test/osgi_req_test.py
+++ test/osgi_req_test.py
@@ -5,27 +5,19 @@ from test_common import *
 
 class TestOSGiReq(unittest.TestCase):
 
-    def tearDown(self):
-        try:
-            shutil.rmtree("/tmp/.javapackages_cache/")
-        except OSError:
-            pass
-
-    @osgireq(["data/osgi/basic/buildroot/usr/share/META-INF/MANIFEST.MF",
-              "data/osgi/basic/buildroot/"])
+    @osgireq(["basic/buildroot/usr/share/META-INF/MANIFEST.MF"])
     def test_basic(self, stdout, stderr, return_value):
         self.assertEqual(return_value, 0, stderr)
         sout = [x for x in stdout.split('\n') if x]
         assertIn(self, "osgi(org.hamcrest.core)", sout)
 
-    @osgireq(["data/osgi/basic_jar/buildroot/usr/lib/basic.jar",
-              "data/osgi/basic_jar/buildroot/"])
+    @osgireq(["basic_jar/buildroot/usr/lib/basic.jar"])
     def test_basic_jar(self, stdout, stderr, return_value):
         self.assertEqual(return_value, 0, stderr)
         sout = [x for x in stdout.split('\n') if x]
         assertIn(self, "osgi(org.hamcrest.core)", sout)
 
-    @osgireq(["data/osgi/empty/META-INF/MANIFEST.MF"])
+    @osgireq(["empty/META-INF/MANIFEST.MF"])
     def test_empty(self, stdout, stderr, return_value):
         self.assertEqual(return_value, 0, stderr)
         sout = [x for x in stdout.split('\n') if x]
