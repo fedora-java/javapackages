3a1f75c31169251da0c98573bf9514efc0407e2b
Michal Srb <msrb@redhat.com>
2014-12-11 13:57:13 +0100

    Fix "TypeError: __init__() got an unexpected keyword argument 'stream'" on Python 2.6
    "stream" argument used to be called "strm" in Python 2.6. Luckily, the
    default value is "sys.stderr", so we don't need this argument at all.

--- python/javapackages/common/util.py
+++ python/javapackages/common/util.py
@@ -115,7 +115,7 @@ def _get_cachedir(path, create_if_not_exists=True):
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler(stream=sys.stderr)
+    handler = logging.StreamHandler()
     formatter = logging.Formatter("[%(levelname)s %(name)s] %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
