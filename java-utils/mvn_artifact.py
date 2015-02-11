6c3d1a682fe6119480964a64277b178d0010f541
Michal Srb <msrb@redhat.com>
2015-02-11 15:38:58 +0100

    Revert "[mvn_artifact] Do not generate requires on optional dependecies"
    This reverts commit d6a077e0bbc68b250ffda47766135730d7d2d11c.

--- java-utils/mvn_artifact.py
+++ java-utils/mvn_artifact.py
@@ -219,9 +219,8 @@ def gather_dependencies(pom_path):
                 d.merge_with(dm)
                 break
 
-    # only non-optional deps with scope "compile" or "runtime" are interesting
-    deps = [x for x in deps if x.scope in ["", "compile", "runtime"]
-            and not x.is_optional()]
+    # only deps with scope "compile" or "runtime" are interesting
+    deps = [x for x in deps if x.scope in ["", "compile", "runtime"]]
 
     return deps
 
