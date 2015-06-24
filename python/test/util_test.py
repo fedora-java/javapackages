75e2b6a6b1ad456122a8fc5b990c3edb7a4edf2b
Michal Srb <msrb@redhat.com>
2015-06-24 08:57:08 +0200

    [test] Fix buildroot location

--- python/test/util_test.py
+++ python/test/util_test.py
@@ -7,7 +7,8 @@ from javapackages.common.util import get_buildroot_files
 class TestUtil(unittest.TestCase):
 
     def test_br_files_basic(self):
-        br_path = os.path.abspath("data/buildroots/br1/")
+        br_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               "data/buildroots/br1/"))
         files = get_buildroot_files(regexp="jar$", buildroot_path=br_path)
         self.assertEqual(1, len(files))
         self.assertTrue(next(iter(files)).endswith("/usr/share/java/regular.jar"))
