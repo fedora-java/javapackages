9b506511ff13c37be64f290e0eb9bbc17fc2266d
Michal Srb <msrb@redhat.com>
2015-09-29 15:16:32 +0200

    Add rpm-build to vagrant_bootstrap.sh

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 dnf update -y
-dnf install -y python{,3} python{,3}-lxml python{,3}-six python{,3}-nose
+dnf install -y rpm-build python{,3} python{,3}-lxml python{,3}-six python{,3}-nose
