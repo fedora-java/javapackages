fdcdff36270a886f21a53f6bafe73506b7ae9bb3
Michal Srb <msrb@redhat.com>
2015-10-06 16:41:15 +0200

    vagrant: add missing dependencies: make, asciidoc, dia

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 dnf update -y
-dnf install -y rpm-build python{,3} python{,3}-lxml python{,3}-six python{,3}-nose
+dnf install -y \
+    rpm-build \
+    make \
+    asciidoc \
+    dia \
+    python{,3} \
+    python{,3}-lxml \
+    python{,3}-six \
+    python{,3}-nose
