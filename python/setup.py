44aceb9a88d92501189e1c19c95b45541128a9e0
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-09-30 10:48:02 +0200

    Update upstream information in setup.py
    java-devel mailing list should not be used as posting to it requires
    subscription.

--- python/setup.py
+++ python/setup.py
@@ -15,10 +15,10 @@ setup(
                 "files for Java packaging in Linux distributions",
     version=__version__,
     license="BSD",
-    download_url="https://fedorahosted.org/released/javapackages/",
-    url="https://git.fedorahosted.org/git/javapackages.git",
+    download_url="https://github.com/fedora-java/javapackages/releases",
+    url="https://github.com/fedora-java/javapackages",
     packages=find_packages(exclude=["test"]),
     test_suite="test",
     maintainer="javapackages maintainers",
-    maintainer_email="java-devel@lists.fedoraproject.org"
+    maintainer_email="javapackages-tools-owner@fedoraproject.org"
 )
