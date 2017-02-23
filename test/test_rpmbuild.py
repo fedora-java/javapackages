00f19efb429d3b20e108a493a1c33d2fdcdc7093
Michael Simacek <msimacek@redhat.com>
2017-02-23 13:47:07 +0100

    Change template expansion syntax from %{foo} to @{foo}
    In order to reduce ambiguity in macro/attrs files.

--- test/test_rpmbuild.py
+++ test/test_rpmbuild.py
@@ -189,9 +189,9 @@ def _prepare_macros():
                         if '/usr/share/java-utils' in line:
                             line = re.sub(r'/usr/share/java-utils',
                                           java_utils, line)
-                        if '%{javadir}-utils' in line:
-                            line = re.sub(r'%\{javadir\}-utils',
+                        if '@{javadir}-utils' in line:
+                            line = re.sub(r'@\{javadir\}-utils',
                                           java_utils, line)
-                        if '%{pyinterpreter}' in line:
-                            line = re.sub(r'%\{pyinterpreter\}', sys.executable, line)
+                        if '@{pyinterpreter}' in line:
+                            line = re.sub(r'@\{pyinterpreter\}', sys.executable, line)
                         rpmmacros.write(line)
