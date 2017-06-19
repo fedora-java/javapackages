0712e97d3f63f6c01a9f9ab27613344dced2bf1b
Roman Vais <rvais@redhat.com>
2017-06-19 11:45:47 +0200

    Add test case for mvn_build.py

--- test/mvn_build_test.py
+++ test/mvn_build_test.py
@@ -1,6 +1,8 @@
 import unittest
 import shutil
 import os
+import sys
+import subprocess
 from test_common import javautils_script, get_actual_args, get_expected_args, \
         get_config_file_list, get_actual_config, get_expected_config, DIRPATH
 from xml_compare import compare_xml_files
@@ -14,6 +16,25 @@ class TestMvnBuild(unittest.TestCase):
         self.workdir = os.path.join(DIRPATH, 'workdir')
         os.mkdir(self.workdir)
         os.chdir(self.workdir)
+
+        # copy the xml file for test_builddep_compression test case
+        builddep_xml = os.path.join(DIRPATH, 'data', 'mvn_build', 'xmvn-builddep.xml')
+        builddep_file = os.path.join(self.workdir, '.xmvn-builddep')
+        shutil.copy(builddep_xml, builddep_file)
+
+        # copy xmvn-builddep script and replace '@' denoted variables
+        # with correct path to the resources
+        original_script = os.path.join(DIRPATH, '..', 'bin', 'xmvn-builddep')
+        self.script = os.path.join(self.workdir, 'script')
+        shutil.copy2(original_script, self.script) # used so file is still executable
+        builddep_py = os.path.join(DIRPATH, '..', 'java-utils', 'builddep.py')
+        with open(self.script, "wt") as wf: 
+            with open(original_script, "rt") as rf: 
+                for line in rf:
+                    if (line.find('@') > -1): 
+                        line.replace("@{pyinterpreter}", sys.executable)
+                        line.replace("@{javadir}-utils/builddep.py", builddep_py)
+                    wf.write(line)
 
     def tearDown(self):
         try:
@@ -31,6 +52,45 @@ class TestMvnBuild(unittest.TestCase):
         self.assertEqual(return_value, 0, stderr)
         self.assertEqual(get_actual_args(),
                 get_expected_args('mvn_build', 'run_no_args'))
+
+    @javautils_script('mvn_build',[])
+    def test_builddep_compression(self, stdout, stderr, return_value):
+        self.assertEqual(return_value, 0, stderr)
+        self.assertEqual(get_actual_args(),
+                get_expected_args('mvn_build', 'run_no_args'))
+
+        log_path = os.path.join(self.workdir, "tmpbuild.log")
+        with open(log_path, 'w') as outfile:
+            outfile.write(stdout)
+
+        with open("tmpout", 'w') as outfile:
+            with open("tmperr", 'w') as errfile:
+                p = subprocess.Popen([self.script, log_path],
+                                     shell=True,
+                                     stdout=outfile,
+                                     stderr=errfile,
+                                     universal_newlines=True)
+                return_value = p.wait()
+
+        with open("tmpout", 'r') as outfile:
+            out = outfile.read()
+        with open("tmperr", 'r') as errfile:
+            err = errfile.read()
+        os.remove('tmpout')
+        os.remove('tmperr')
+
+        builddep_res = os.path.join(DIRPATH, 'data', 'mvn_build', 'builddep.result')
+        result = ""
+        with open(builddep_res, 'rt') as rf:
+            result = rf.read()
+
+        self.assertEqual(return_value, 0, err)
+        self.assertEqual(out, result,
+                          "expected:\n---\n" 
+                        + result + "\ngained:\n---\n"
+                        + out + "\nparsed from output:\n---\n"
+                        + stdout)
+        self.assertEqual(err, "", err)
 
     @javautils_script('mvn_build',['-b', ])
     def test_bootstrap(self, stdout, stderr, return_value):
