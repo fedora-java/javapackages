616500f5cf7bb0b3b124d228887d6b89a2b59bfc
Marián Konček <marian.koncek@mailbox.org>
2023-03-03 11:14:49 +0100

    Remove annotation removal scripts

--- test/data/java_remove_symbols/Class2.java
+++
@@ -1,8 +0,0 @@
-import java.lang.Runnable;
-import java.util.List;
-import static java.util.*;
-import static java.lang.String.valueOf;
-import com.google.common.util.concurrent.Service;
-
-class Class2 {
-}
